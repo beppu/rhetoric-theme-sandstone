8c6465109e1cc76bc2a84d3ad405277835e4c7e5
John Beppu <john.beppu@gmail.com>
2011-04-27 17:21:53 -0700

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 322ac92..7ac8f26 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension Rhetoric::Theme::Sandstone
    
    +0.04 Wed Apr 27 17:21:29 PDT 2011
    +  - case insensitive filesystems deceived me
    +
     0.03 Wed Apr 27 17:13:19 PDT 2011
       - comment formatting

--- lib/Rhetoric/Theme/SandStone.pm
+++ lib/Rhetoric/Theme/SandStone.pm
@@ -1,7 +1,7 @@
 package Rhetoric::Theme::SandStone;
 use strict;
 use warnings;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 use Squatting::View;
 use Method::Signatures::Simple;
 use Data::Dump 'pp';
